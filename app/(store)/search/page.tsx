9a102c67f9f2a3f72169764252c64984c65287be
mhasnain23 <tryfrelancer23@gmail.com>
2025-01-04 14:56:07 +0500

    Implement search results display and enhance product search functionality

--- app/(store)/search/page.tsx
+++ app/(store)/search/page.tsx
@@ -1,3 +1,4 @@
+import ProductGrid from "@/components/ProductGrid";
 import { searchProductsByName } from "@/sanity/lib/products/searchProductsByName";
 import React from "react";
 
@@ -11,7 +12,31 @@ const SearchPage = async ({
   const { query } = await searchParams;
   const products = await searchProductsByName(query);
 
-  return <div>SearchPage for {query}</div>;
+  if (!products.length) {
+    return (
+      <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+        <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
+          <h1 className="text-2xl font-bold mb-6 text-center">
+            No products found for: {query}
+          </h1>
+          <p className="text-gray-600 text-center">
+            Try searching with different keywords
+          </p>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
+        <h1 className="text-3xl font-bold mb-6 text-center">
+          Search Results for: {query}
+        </h1>
+        <ProductGrid products={products} />
+      </div>
+    </div>
+  );
 };
 
 export default SearchPage;
--- sanity/lib/products/searchProductsByName.ts
+++ sanity/lib/products/searchProductsByName.ts
@@ -1,9 +1,26 @@
 import { defineQuery } from "next-sanity"
+import { sanityFetch } from "../live"
 
-export const searchProductsByName = (searchParams: string) => {
+export const searchProductsByName = async (searchParam: string) => {
     const PRODUCT_SEARCH_QUERY = defineQuery(`
     *[
-        _type == "product" && name match $searchParams
+        _type == "product" 
+        && name match $searchParam
     ] | order(name asc)
     `)
+
+    try {
+        const products = await sanityFetch({
+            query: PRODUCT_SEARCH_QUERY,
+            params: {
+                searchParam: `${searchParam}*` // append wildcard to search for partial matches
+            }
+        })
+
+        return products.data || []
+    } catch (error) {
+        console.log("Error fetching products by name", error);
+        return [];
+
+    }
 }
