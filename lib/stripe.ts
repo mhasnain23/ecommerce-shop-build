0abff14918ed0034f50542db7be7f96ba2d1403c
mhasnain23 <tryfrelancer23@gmail.com>
2025-01-07 14:01:06 +0500

    Add Stripe dependency to package.json and package-lock.json

---
+++ lib/stripe.ts
@@ -0,0 +1,13 @@
+import { exportTraceState } from "next/dist/trace";
+import Stripe from "stripe";
+
+
+if (!process.env.STRIPE_SECRET_KEY) {
+    throw new Error("STRIPE_SECRET_KEY is not defined");
+}
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+    apiVersion: "2024-12-18.acacia",
+})
+
+export default stripe;
