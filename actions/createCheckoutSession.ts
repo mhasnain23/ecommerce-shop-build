99aa43c7d4000eb62775096a342cd8ecce77db77
mhasnain23 <tryfrelancer23@gmail.com>
2025-01-10 14:41:13 +0500

    updated

--- actions/createCheckoutSession.ts
+++ actions/createCheckoutSession.ts
@@ -35,9 +35,7 @@ export async function createCheckoutSession(items: GroupedBasketItem[], metadata
             customerId = customers.data[0].id;
         }
 
-        const baseUrl = process.env.NODE_ENV === 'production'
-            ? `https://${process.env.VERCEL_URL}`
-            : process.env.NEXT_PUBLIC_BASE_URL;
+        const baseUrl = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASE_URL : "";
 
         const successUrl = `${baseUrl}/success?session_id={CHECKOUT_SESSION_ID}&orderNumber=${metadata.orderNumber}`;
         const cancelUrl = `${baseUrl}/basket`;
